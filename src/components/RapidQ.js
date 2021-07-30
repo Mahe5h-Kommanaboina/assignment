54550e099688b7977016e147b20627de72800754
Mahe5h-Kommanaboina <83692984+Mahe5h-Kommanaboina@users.noreply.github.com>
2021-07-30 21:02:12 +0530

    updated with user logic

--- src/components/RapidQ.js
+++ src/components/RapidQ.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Header from './Header'
+import axios from 'axios'
 
 
 
 const RapidQ = () => {
+    const [users,setUsers] = useState([])
+
+    
+     
+    useEffect( () => {
+        axios.get('https://api.github.com/users').then(
+        res => setUsers(res.data)
+
+    )},[]) 
+    
+
+    
     return (
     <React.Fragment>
         <div className="container-sm">
@@ -12,8 +25,20 @@ const RapidQ = () => {
                     <div className="card">
                         
                         <div className="card-body">
-                            <Header></Header>
-                            <Header></Header>
+                            {users.map( user => {
+                                if(user.id %2 ===0){
+                                    return(
+                                        <div key={user.id}>
+                                            <Header user={user.login} avatar={user.avatar_url}></Header>
+                                        </div>
+                                    )
+                                }})}
+
+                            
+                            
+                            
+                            
+                            
                             <div style={{textAlign:"center",color:"green"}}>
                                 
                                     <button type="button" className="btn btn-success btn-sm">Add</button>
@@ -25,7 +50,14 @@ const RapidQ = () => {
                     <div className="card">
                             
                             <div className="card-body">
-                                <Header></Header>
+                            {users.map( user => {
+                                if(user.id %2 ===1){
+                                    return(
+                                        <div key={user.id}>
+                                            <Header user={user.login} avatar={user.avatar_url}></Header>
+                                        </div>
+                                    )
+                                }})}
                                 <div style={{textAlign:"center"}}>
                                 
                                     <button type="button" className="btn btn-success btn-sm">Add</button>
